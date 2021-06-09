6f5556a662c15aef96d31a9bdcee7e79ac5f1cea
AlecGamble <Alec.Gamble@live.co.uk>
2021-06-10 00:41:32 +0100

    ScreenCapture Utility Added - Small Tweaks to demo scene

---
+++ Potion Panic/Assets/Code/Editor/ScreenshotTaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ScreenshotTaker
+{
+	[MenuItem("Screen/Capture")]
+	public static void CaptureScreenshot()
+	{
+		string date = System.DateTime.Now.ToString();
+		date = date.Replace("/","-");
+		date = date.Replace(" ","_");
+		date = date.Replace(":","-");
+		ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/Potion_Panic_"+date+".png");
+	}
+}
