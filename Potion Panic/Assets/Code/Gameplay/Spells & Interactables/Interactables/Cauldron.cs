a50ae6e8173a2f9b217de7746452b1e5c393cd11
Mani <manikkanaga@gmail.com>
2021-08-02 16:59:07 +0100

    Added some UI elements

--- Potion Panic/Assets/Code/Gameplay/Spells & Interactables/Interactables/Cauldron.cs
+++ Potion Panic/Assets/Code/Gameplay/Spells & Interactables/Interactables/Cauldron.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cauldron : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class Cauldron : MonoBehaviour
     // Variables for post consumption of ingredients
     private bool ingredientLimitReached;
     private bool spellBrewed;
+    private float brewDelay = 0.25f;
 
     public List<Ingredient> consumedIngredients = new List<Ingredient>();
     public List<Spell_SO> spellPool; // Should be Spell Prefab
@@ -29,10 +31,13 @@ public class Cauldron : MonoBehaviour
     public delegate void OnFailure();
     public event OnFailure OnFailureEvent;
 
+    // UI
+    public List<GameObject> ingredientIconMasks; // Circle Mask
+    public List<GameObject> ingredientIcons; // Actual Image
+
     // Start is called before the first frame update
     //void Start()
     //{
-
     //}
 
     // Update is called once per frame
@@ -41,7 +46,18 @@ public class Cauldron : MonoBehaviour
         if (droppingIngredient != null)
             ConsumeIngredient();
         else if (ingredientLimitReached)
-            BrewSpell();
+        {
+            brewDelay -= Time.deltaTime;
+
+            if(brewDelay <= 0)
+            {
+                BrewSpell();
+                brewDelay = 0.25f;
+            }
+        }
+
+
+        ManageUI();
     }
 
     void ConsumeIngredient()
@@ -103,6 +119,21 @@ public class Cauldron : MonoBehaviour
         ingredientLimitReached = false;
         spellBrewed = false;
         consumedIngredients.Clear();
+    }
+
+    void ManageUI()
+    {
+        if (consumedIngredients.Count > 0)
+        {
+            for (int i = 0; i < consumedIngredients.Count; i++)
+            {
+                ingredientIconMasks[i].gameObject.SetActive(true);
+                ingredientIcons[i].GetComponent<Image>().sprite = consumedIngredients[i].ingredientInfo.ingredientPreview;
+            }
+        }
+        else
+            for (int i = 0; i < ingredientIconMasks.Count; i++)
+                ingredientIconMasks[i].SetActive(false);
     }
 
     public void AssignDroppingIngredient(Ingredient newIngredient)
--- Potion Panic/Assets/Code/UI/PlayerInfoHUD.cs
+++ Potion Panic/Assets/Code/UI/PlayerInfoHUD.cs
@@ -7,9 +7,18 @@ public class PlayerInfoHUD : MonoBehaviour
 {
     private PlayerController player;
 
-    // UI
+    // KIllS
     public Text killCounter;
-    public Text spellInfo;
+
+    // HEALTH
+    public Image healthBar;
+
+    // SPELL
+    public GameObject spellInfo;
+    public Image spellIcon;
+    public Text numOfUses;
+
+    public bool leftAligned;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +29,26 @@ public class PlayerInfoHUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        killCounter.text = player.kills.ToString();
+        if (leftAligned)
+            killCounter.text = "x " + player.kills.ToString();
+        else
+            killCounter.text = player.kills.ToString() + " x";
 
         if (player.hasSpell)
-            spellInfo.text = "Spell: " + player.spellInfo.Name + " (" + player.spellUses + ")";
-        else if (player.myCauldron.consumedIngredients.Count < 3)
-            spellInfo.text = "Ingredients Used: " + player.myCauldron.consumedIngredients.Count;
+        {
+            spellInfo.SetActive(true);
+
+            spellIcon.sprite = player.spellInfo.spellPreview;
+
+            if (leftAligned)
+                numOfUses.text = "x " + player.spellUses;
+            else
+                numOfUses.text = player.spellUses + " x";
+        }
+        else
+            spellInfo.SetActive(false);
+
+        healthBar.fillAmount = player.health / 100;
     }
 
 }
